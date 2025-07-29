60cf8be05a629bc5aa811c266e47c6793bddcc7b
Bao <baodev@gmail.com>
2025-07-30 03:52:11 +0700

    Update MongoDB URI to use environment variable

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose")
+﻿const mongoose = require("mongoose")
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/library-management", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    console.log("MongoDB connected successfully")
-  } catch (error) {
-    console.error("Database connection error:", error)
-    process.exit(1)
-  }
+    try {
+        await mongoose.connect("mongodb+srv://fuibui3:123456%40@cluster0.jnhped4.mongodb.net/library-management?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("MongoDB connected successfully")
+    } catch (error) {
+        console.error("Database connection error:", error)
+        process.exit(1)
+    }
 }
 
 connectDB()
