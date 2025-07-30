d2ccdef26a4d40602fd8ac69ef716cbec0e29278
Bao <baodev@gmail.com>
2025-07-30 16:56:23 +0700

    Deploy version

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
-﻿// config/database.js
-const mongoose = require("mongoose")
+﻿const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://fuibui3:123456%40@cluster0.jnhped4.mongodb.net/library-management?retryWrites=true&w=majority", {
+        await mongoose.connect("mongodb+srv://fuibui3:123456%40@cluster0.jnhped4.mongodb.net/library-management?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        })
-        console.log("✅ MongoDB connected successfully")
+            serverSelectionTimeoutMS: 10000,
+            ssl: true
+        });
+        console.log("MongoDB connected successfully");
     } catch (error) {
-        console.error("❌ Database connection error:", error)
-        process.exit(1)
+        console.error("Database connection error:", error);
+        process.exit(1);
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
